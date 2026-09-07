1f445f0182f3b1bded78d34118ffc553f2c40f56
s-byu-acc <sdamas1@byupathway.edu>
2026-09-07 07:29:59 -0300

    Connect app to MongoDB

--- src/db/connect.js
+++ src/db/connect.js
@@ -10,7 +10,7 @@ const connectToDb = async () => {
 
   const client = new MongoClient(connectionString);
   await client.connect();
-  database = client.db(process.env.MONGODB_DB_NAME || 'practice');
+  database = client.db(process.env.MONGODB_DB_NAME);
   return database;
 };
 
