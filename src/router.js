5f27eea571ea44d026da006cb722785c9171c1e2
s-byu-acc <sdamas1@byupathway.edu>
2026-09-08 13:07:58 -0300

    Add swagger documentation.

--- src/router.js
+++ src/router.js
@@ -3,7 +3,98 @@ import { getBooksHandler, getBookByIdHandler } from "./controllers/books.js";
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get all books
+ *     responses:
+ *       200:
+ *         description: A list of books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     example: "1"
+ *                   title:
+ *                     type: string
+ *                     example: "The Great Gatsby"
+ *                   author:
+ *                     type: string
+ *                     example: "F. Scott Fitzgerald"
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ */
 router.get("/books", getBooksHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get a book by ID
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the book
+ *         schema:
+ *           type: string
+ *         example: "1"
+ *     responses:
+ *       200:
+ *         description: Book found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   example: "1"
+ *                 title:
+ *                   type: string
+ *                   example: "The Great Gatsby"
+ *                 author:
+ *                   type: string
+ *                   example: "F. Scott Fitzgerald"
+ *       404:
+ *         description: Book not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Book not found
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ */
 router.get("/books/:id", getBookByIdHandler);
 
 export default router;
