7d1abc58c88ce0a1a57d4e11aaa8b530375921a5
s-byu-acc <sdamas1@byupathway.edu>
2026-09-12 16:48:21 -0300

    Update books API

--- src/models/books.js
+++ src/models/books.js
@@ -1,13 +1,79 @@
-import { getDb } from "../db/connect.js";
+import { getDb } from '../db/connect.js';
 
 const getAllBooks = async () => {
-    const books = await getDb().collection("books").find().toArray();
-    return books;
+  const db = getDb();
+  const collection = db.collection('books');
+  const books = await collection.find({}).toArray();
+
+  return books;
 };
 
-const getBookById = async (bookId) => {
-    const book = await getDb().collection("books").findOne({ id: bookId });
-    return book;
-}
+const getBookById = async (id) => {
+  const db = getDb();
+  const collection = db.collection('books');
+  const book = await collection.findOne({ id });
 
-export { getAllBooks, getBookById };
+  return book;
+};
+
+const createBook = async (book) => {
+  const db = getDb();
+  const collection = db.collection('books');
+
+  const bookDocument = {
+    id: book.id,
+    authorId: book.authorId,
+    title: book.title,
+    publicationDate: book.publicationDate,
+  };
+
+  await collection.insertOne(bookDocument);
+
+  return bookDocument;
+};
+
+const updateBook = async (id, book) => {
+  const db = getDb();
+  const collection = db.collection('books');
+
+  const updatedBook = {
+    id,
+    authorId: book.authorId,
+    title: book.title,
+    publicationDate: book.publicationDate,
+  };
+
+  const result = await collection.updateOne(
+    { id },
+    {
+      $set: {
+        authorId: updatedBook.authorId,
+        title: updatedBook.title,
+        publicationDate: updatedBook.publicationDate,
+      },
+    },
+  );
+
+  if (result.matchedCount === 0) {
+    return null;
+  }
+
+  return updatedBook;
+};
+
+const deleteBook = async (id) => {
+  const db = getDb();
+  const collection = db.collection('books');
+
+  const result = await collection.deleteOne({ id });
+
+  return result.deletedCount > 0;
+};
+
+export {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+};
